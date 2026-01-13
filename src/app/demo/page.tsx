61899ed4cfe79714363c570ff22fa5de0f46fbf1
Jorge Matos <jisapjisap@gmail.com>
2026-01-13 20:02:55 +0100

    demo inngest

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -7,16 +7,28 @@ import { useState } from "react"
 export default function Demo() {
 
   const [loading, setLoading] = useState(false)
+  const [loading2, setLoading2] = useState(false)
   
   const handleBlocking = async () => {
+    setLoading(true)
     await fetch("/api/demo/blocking", { method: "POST" })
     setLoading(false)
+  }
+  
+  const handleBackground = async () => {
+    setLoading2(true)
+    await fetch("/api/demo/background", { method: "POST" })
+    setLoading2(false)
   }
   
   return (
     <div className='p-8 space-x-4'>
       <Button onClick={handleBlocking} disabled={loading}>
         {loading ? "Loading..." : "Blocking"}
+      </Button>
+
+      <Button onClick={handleBackground} disabled={loading}>
+        {loading ? "Loading..." : "Background"}
       </Button>
     </div>
   )
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,17 @@
+import { google } from "@ai-sdk/google";
 import { inngest } from "./client";
+import { generateText } from "ai";
 
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
+export const demoGenerate = inngest.createFunction(
+  { id: "demo-generate" },
+  { event: "demo/generate" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
-  },
-);
+    await step.run("generate-text", async () => {
+      return await generateText({
+        model: google("gemini-2.5-flash"),
+        prompt: "Write a vegetarian lasagna recipe for 4 people",
+      })
+    })
+  }
+)
+  
