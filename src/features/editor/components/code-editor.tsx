e04885e82f75ec105aa66ba66deb05bbdeb6d602
Jorge Matos <jisapjisap@gmail.com>
2026-01-23 18:18:42 +0100

    implement theme to Editor View

--- src/features/editor/components/code-editor.tsx
+++ src/features/editor/components/code-editor.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef } from 'react'
 import { basicSetup, EditorView } from 'codemirror'
 import { javascript } from "@codemirror/lang-javascript"
+import { oneDark } from "@codemirror/theme-one-dark"
+import { customTheme } from '../extensions/theme'
 
 
 export const CodeEditor = () => {
@@ -12,9 +14,22 @@ export const CodeEditor = () => {
     if (!editorRef.current) return;
 
     const view = new EditorView({
-      doc: "Star document",
+      doc: `
+        const Counter = () => {
+          const [count, setCount] = useState(0);
+          return (
+            <div>
+              <p>Count: {count}</p>
+              <button onClick={() => setCount(count + 1)}>Increment</button>
+              <button onClick={() => setCount(count - 1)}>Decrement</button>
+            </div>
+          );
+        };
+      `,
       parent: editorRef.current,
       extensions: [
+        oneDark,
+        customTheme,
         basicSetup,
         javascript({ typescript: true })
       ]
