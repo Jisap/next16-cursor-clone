e1c5ba4aa9730bbdd0fd70b13f90fdd4249678c8
Jorge Matos <jisapjisap@gmail.com>
2026-02-06 19:14:18 +0100

    feat: Create `ProjectsView` component to manage project creation, import from GitHub, and display project lists with associated command dialogs and keyboard shortcuts.

--- src/features/projects/components/projects-view.tsx
+++ src/features/projects/components/projects-view.tsx
@@ -7,16 +7,10 @@ import { SparkleIcon } from "lucide-react";
 import { Poppins } from "next/font/google";
 import { FaGithub } from "react-icons/fa"
 import { ProjectstList } from "./projects-lists";
-import { useCreateProject } from "../hooks/use-projects";
-import {
-  adjectives,
-  animals,
-  colors,
-  uniqueNamesGenerator,
-} from "unique-names-generator"
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { ProjectsCommandDialog } from "./projects-command-dialog";
 import { ImportGithubDialog } from "./import-github-dialog";
+import { NewProjectDialog } from "./new-project-dialog";
 
 
 const font = Poppins({
@@ -28,18 +22,7 @@ export const ProjectsView = () => {
 
   const [commandDialogOpen, setCommandDialogOpen] = useState(false);
   const [importDialogOpen, setImportDialogOpen] = useState(false);
-  const createProject = useCreateProject();
-
-  const handleCreateProject = useCallback(() => {
-    const projectName = uniqueNamesGenerator({
-      dictionaries: [adjectives, animals, colors],
-      separator: "_",
-      length: 3,
-    })
-    createProject({
-      name: projectName
-    })
-  }, [createProject])
+  const [newProjectDialogOpen, setNewProjectDialogOpen] = useState(false);
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
@@ -53,7 +36,7 @@ export const ProjectsView = () => {
       }
       if (event.key === "j" && (event.metaKey || event.ctrlKey)) {
         event.preventDefault()
-        handleCreateProject()
+        setNewProjectDialogOpen(true)
       }
     }
 
@@ -62,7 +45,9 @@ export const ProjectsView = () => {
     return () => {
       window.removeEventListener("keydown", handleKeyDown)
     }
-  }, [handleCreateProject])
+  }, [])
+
+
 
   return (
     <>
@@ -74,6 +59,11 @@ export const ProjectsView = () => {
       <ImportGithubDialog
         open={importDialogOpen}
         onOpenChange={setImportDialogOpen}
+      />
+
+      <NewProjectDialog
+        open={newProjectDialogOpen}
+        onOpenChange={setNewProjectDialogOpen}
       />
 
 
@@ -97,7 +87,7 @@ export const ProjectsView = () => {
             <div className="grid grid-cols-2 gap-2">
               <Button
                 variant="outline"
-                onClick={handleCreateProject}
+                onClick={() => setNewProjectDialogOpen(true)}
                 className="h-full items-start p-4 bg-background border flex flex-col gap-6 rounded-none"
               >
                 <div className="flex items-center justify-between w-full">
